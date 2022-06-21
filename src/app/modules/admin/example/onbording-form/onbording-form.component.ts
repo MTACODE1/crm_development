3afbe1831cfb27db45dda62c57f4b9be1674e249
shital <webdevelopersuc@gmail.com>
2022-06-21 18:28:57 +0530

    remove ngmodel and added confirmation

--- src/app/modules/admin/example/onbording-form/onbording-form.component.ts
+++ src/app/modules/admin/example/onbording-form/onbording-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FuseConfirmationService } from '@fuse/services/confirmation';
 import { OnboardingItems, ONBOARDING_FORM_ITEMS } from './onborading-item';
 
 @Component({
@@ -12,7 +13,7 @@ export class OnbordingFormComponent implements OnInit {
   task: OnboardingItems[] = [];
 
   constructor(@Inject(MAT_DIALOG_DATA) public readonly data: any, 
-    public readonly dialogRef: MatDialogRef<OnbordingFormComponent>) 
+    public readonly dialogRef: MatDialogRef<OnbordingFormComponent>, private _fuseConfirmationService: FuseConfirmationService) 
     { }
 
   ngOnInit(): void {
@@ -29,8 +30,31 @@ export class OnbordingFormComponent implements OnInit {
     console.log(event)
   }
 
-  public toggleClass(item: OnboardingItems): void {
+  private toggleClass(item: OnboardingItems): void {
     item.active = !item.active;
+  }
+
+  toggleData(item:OnboardingItems, status:boolean) {
+    const message = status?`Add <b>${item.text}</b> to onboarding for client name`:`Remove <b>${item.text}</b> from onboarding for client name?`
+    const dialogRef = this._fuseConfirmationService.open({
+      title : 'Are you sure?',
+      message : message,
+      dismissible: true,
+      actions:{
+        cancel:{
+          label:'No'
+        },
+        confirm :{
+          label: 'Yes',
+          color: 'warn'
+        }
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if(result === 'confirmed') {
+        this.toggleClass(item);
+      }
+    });
   }
 
 }
