8f2e9a905a11836eb17da6870eb9dd05ded5213e
shital <webdevelopersuc@gmail.com>
2022-07-19 14:34:50 +0530

    task completion log UI

--- src/app/modules/admin/reports/completed-task/completed-task.component.ts
+++ src/app/modules/admin/reports/completed-task/completed-task.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TasksMockApi } from 'app/mock-api/apps/tasks/api';
+import { SalesflowUser } from 'app/mock-api/apps/tasks/data';
+import { Subject, takeUntil } from 'rxjs';
 
 export interface PeriodicElement {
   name: string;
@@ -13,8 +16,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {id: 1, name: 'Rayan', company: 'xero', type: 'bookkeeping' ,date: '11/11/2022', time:'10:20', task:'task here'},
   {id: 2, name: 'David', company: 'xero', type: 'vat' , date:  '11/11/2022', time:'10:20', task:'task here'},
   {id: 3, name: 'Tariq', company: 'xero', type: 'bookkeeping' , date:  '11/11/2022', time:'10:20', task:'task here'},
-  {id: 4, name: 'Rizwan', company: 'xero', type: 'bookkeeping' , date:  '11/11/2022', time:'10:20', task:'task here'},
-  {id: 5, name: 'Shital', company: 'xero', type: 'self assesmen' , date:  '11/11/2022', time:'10:20', task:'task here'},
+  {id: 4, name: 'Rizwan', company: 'xero', type: 'account' , date:  '11/11/2022', time:'10:20', task:'task here'},
+  {id: 5, name: 'Shital', company: 'xero', type: 'assessment' , date:  '11/11/2022', time:'10:20', task:'task here'},
 ];
 
 @Component({
@@ -25,9 +28,23 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class CompletedTaskComponent implements OnInit {
   displayedColumns: string[] = ['username','company', 'taskType', 'task', 'date', 'time'];
   dataSource = ELEMENT_DATA;
-  constructor() { }
+  public users: SalesflowUser[] = [];
+  private readonly destroyer$: Subject<void> = new Subject();
+
+  constructor(private taskService: TasksMockApi) { }
 
   ngOnInit(): void {
+    this.getUserList();
   }
 
+  ngOnDestroy(): void {
+    this.destroyer$.next();
+    this.destroyer$.complete();
+  }
+
+  private getUserList(): void {
+    this.taskService.userList({}).pipe(takeUntil(this.destroyer$)).subscribe(response => {
+      this.users = response;
+    });
+  }
 }
