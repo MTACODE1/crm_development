d312a8ed703b4a9a81fd3f2d8ede3cb04e2242ce
shital <webdevelopersuc@gmail.com>
2022-08-22 21:11:08 +0530

    job manager page updates

--- src/app/modules/admin/job-manager/job-manager.component.ts
+++ src/app/modules/admin/job-manager/job-manager.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { PeriodicElement } from 'app/mock-api/apps/reports/report-data';
 import { ReportsService } from 'app/mock-api/apps/reports/reports.service';
 import { jobType } from 'app/mock-api/apps/tasks/data';
+import { PaginatorComponent } from 'app/shared/pagination/paginator/paginator.component';
 import { Subject, takeUntil } from 'rxjs';
 
 
@@ -12,8 +13,9 @@ import { Subject, takeUntil } from 'rxjs';
   encapsulation: ViewEncapsulation.None,
 })
 export class JobManagerComponent implements OnInit {
+  @ViewChild(PaginatorComponent , {static:false}) paginatorComponent:PaginatorComponent;
   totalLogCount: number;
-  displayedColumns: string[] = ['job_type', 'client_name','logo', 'period_end', 'mta_deadline', 'statutory_deadline', 'job_stage', 'client_manager', 'job_assignee', 'notes'];
+  displayedColumns: string[] = ['job_type', 'client_name','logo', 'period_end', 'mta_deadline', 'statutory_deadline', 'job_stage','current_task', 'client_manager', 'job_assignee', 'notes'];
   userTerm: string;
   public ProcessTypeEnum = jobType;
   
@@ -22,6 +24,8 @@ export class JobManagerComponent implements OnInit {
   public paginationConfig = {
     limit: 10,
     offset: 0,
+    // page:1
+    
   }
   private readonly destroyer$: Subject<void> = new Subject();
 
@@ -31,7 +35,13 @@ export class JobManagerComponent implements OnInit {
     this.getJobMangerList();
   }
   private getJobMangerList(): void {
-    this.reportService.getJobManagerData({}).pipe(takeUntil(this.destroyer$)).subscribe(response => {
+    const params = {
+      limit: this.paginationConfig.limit,
+      offset: this.paginationConfig.offset,
+      // page: this.paginationConfig.page,
+      
+    }
+    this.reportService.getJobManagerData(params).pipe(takeUntil(this.destroyer$)).subscribe(response => {
    this.jobManagerList = response['rows'];
    this.totalLogCount = response['total_count'];
     });
@@ -46,9 +56,11 @@ export class JobManagerComponent implements OnInit {
       ...event,
       user: this.userTerm,
     }
-    this.reportService.getJobManagerData(params).subscribe(response => {
-      this.jobManagerList = response['rows'];
-    });
+    // this.reportService.getJobManagerData(params).subscribe(response => {
+    //   this.jobManagerList = response['rows'];
+    // });
+    this.getJobMangerList();
   }
 
 }
+
